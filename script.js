a9aff3a4565a5f293b66d6e65b98b8d5a956d576
Muhamad Rafli <85044079+mura-wx@users.noreply.github.com>
2024-08-21 02:56:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,104 +5,75 @@ const result = document.getElementById("result");
 const kodcontent = document.getElementById("kod");
 let name;
 const min = 1;
-const max = 27;
+const max = 60;
 let random;
-let kodam;
+
+const khodamList = [
+  "Ular Naga",
+  "Buaya Putih",
+  "Bhatara Karang",
+  "Harimau Putih",
+  "Sulaiman",
+  "Semar",
+  "Nyi Roro Kidul",
+  "Banaspati",
+  "Jin Ifrit",
+  "Siluman Ular",
+  "acan Kumbang",
+  "Garuda",
+  "Kuda Sembrani",
+  "Ratu Pantai Selatan",
+  "Dewi Sri",
+  "Hanoman",
+  "Barong",
+  "Buto Ijo",
+  "Kera Sakti",
+  "Dewi Lanjar",
+  "Prabu Siliwangi",
+  "ewi Kwan Im",
+  "Raja Jin",
+  "Dewi Durga",
+  "Naga Geni",
+  "Ratu Kidul",
+  "Dewi Tara",
+  "Raja Brawijaya",
+  "Dewi Ratih",
+  "Si Buta dari Gua Hantu",
+  "Pocong Ngantuk",
+  "Genderuwo Nyanyi Dangdut",
+  "Kuntilanak Penakut",
+  "Wewe Gombel Penjual Cilok",
+  "Jin Cilik Pencinta Upin Ipin",
+  "Tuyul Malas Bekerja",
+  "Pocong Fashionista",
+  "Genderuwo Penari Jaipong",
+  "Kuntilanak Penggemar K-Pop",
+  "Wewe Gombel Penjual Gorengan",
+  "Pocong Siluman",
+  "Tuyul Pencinta Es Krim",
+  "Pocong Youtuber",
+  "Genderuwo Stand Up Comedian",
+  "Kuntilanak TikTokers",
+  "Wewe Gombel Influencer",
+  "Jin Cilik Hacker",
+  "Tuyul Trader Saham",
+  "Pocong Vlogger",
+  "Genderuwo Content Creator",
+  "Kuntilanak Ngantuk",
+  "Wewe Gombel Beauty Vlogger",
+  "Jin Cilik Youtuber Gaming",
+  "Tuyul Investor Bitcoin",
+  "Pocong Nyanyi Dangdut",
+  "Genderuwo Penari Tiktok",
+  "Kuntilanak Gamers",
+  "Jin Lampu Aladin Versi Cimahi",
+  "Banaspati Penjaga Warteg",
+  "Siluman Ular Penjual Cilok",
+];
 
 submit.onclick = function () {
   name = document.getElementById("input").value;
   random = Math.floor(Math.random() * max) + min;
-  if (name == "rafli") {
-    kodam = "Raja Iblis LUCIFER";
-    result.textContent = `Hai ${name} kodam mu adalah:`;
-    kodcontent.textContent = kodam;
-  } else {
-    switch (random) {
-      case 1:
-        kodam = "Kunti ijo";
-        break;
-      case 2:
-        kodam = "Gunduruwo emas";
-        break;
-      case 3:
-        kodam = "Harimau jawa";
-        break;
-      case 4:
-        kodam = "Macan tutul";
-        break;
-      case 5:
-        kodam = "Tuyul kembar";
-        break;
-      case 6:
-        kodam = "kosong";
-        break;
-      case 7:
-        kodam = "Badak betawi";
-        break;
-      case 8:
-        kodam = "Kucing Oren";
-        break;
-      case 9:
-        kodam = "Burung Hantu";
-        break;
-      case 10:
-        kodam = "Cumi-Cumi";
-        break;
-      case 11:
-        kodam = "Kuda Nil";
-        break;
-      case 12:
-        kodam = "kupu-Kupu";
-        break;
-      case 13:
-        kodam = "Nasi Goreng";
-        break;
-      case 14:
-        kodam = "Bakso Aci";
-        break;
-      case 15:
-        kodam = "Mie ayamm";
-        break;
-      case 16:
-        kodam = "Es Krim";
-        break;
-      case 17:
-        kodam = "Kopi Susu";
-        break;
-      case 18:
-        kodam = "Jam Dinding";
-        break;
-      case 19:
-        kodam = "Bantal Guling";
-        break;
-      case 20:
-        kodam = "Payung Hujan";
-        break;
-      case 21:
-        kodam = "Kacamata hitam";
-        break;
-      case 22:
-        kodam = "Kaos Kaki";
-        break;
-      case 23:
-        kodam = "Bintang kejora";
-        break;
-      case 24:
-        kodam = "Awan Putih";
-        break;
-      case 25:
-        kodam = "Ombak laut";
-        break;
-      case 26:
-        kodam = "Hujan Rintik";
-        break;
-      case 27:
-        kodam = "Pelangi";
-        break;
-    }
-
-    result.textContent = `Hai ${name} kodam mu adalah:`;
-    kodcontent.textContent = kodam;
-  }
+  result.textContent = `${name} dialah kodam kamu`;
+  kodcontent.textContent = khodamList[random];
 };
-
